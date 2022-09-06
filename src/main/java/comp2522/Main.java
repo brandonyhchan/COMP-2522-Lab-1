91c8e54147a68ff19978dd0c71799f0960b7587e
Brandon Chan <brandonchan45@gmail.com>
2022-09-06 10:29:12 -0700

    Updated code to comply with checkstyle

--- src/main/java/comp2522/Main.java
+++
@@ -1,7 +0,0 @@
-package comp2522;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("My favourite basketball team is the Miami Heat");
-    }
-}
---
+++ src/main/java/org/comp2522/Main.java
@@ -0,0 +1,9 @@
+package org.comp2522;
+
+/**
+ * This is the main method running the program.
+ */
+public class Main { public static void main(String[] args) {
+    System.out.println("My favourite basketball team is the Miami Heat");
+  }
+}
